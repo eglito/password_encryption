2d6ff67fb647a14bcb5d6590ad95ce33f2fc463d
eglito <aleeglito@gmail.com>
2025-02-07 21:16:55 -0300

    Criacao da classe UserController
    Recebe e enviar requisições. Possui os metodos listall, save e passwordValidation

---
+++ src/main/java/org/example/adapter/controller/UserController.java
@@ -0,0 +1,61 @@
+package org.example.adapter.controller;
+
+import org.example.adapter.UserRepository;
+import org.example.entity.UserEntity;
+import org.example.services.PasswordHash;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+    private PasswordHash passwordHash;
+
+    public UserController(UserRepository repository, PasswordHash passwordHash){
+        this.repository = repository;
+        this.passwordHash = passwordHash;
+    }
+
+    @GetMapping("/listall")
+    public ResponseEntity<List<UserEntity>> llistAll(){
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<UserEntity> save(@RequestBody UserEntity userEntity, String id) throws IllegalAccessException {
+
+        String passwordCrypt = this.passwordHash.hashing(userEntity.getPassword());
+        UserEntity user = new UserEntity(userEntity.getLogin(), passwordCrypt);
+        return ResponseEntity.ok(repository.save(user));
+    }
+
+    @GetMapping("/validation")
+    public ResponseEntity<Boolean> passwordValidation(@RequestParam String login, @RequestParam String password) {
+
+        Optional<UserEntity> optionalUser = repository.findByLogin(login);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        UserEntity user = optionalUser.get();
+        boolean valid = this.passwordHash.matches(password, user.getPassword());
+
+        HttpStatus status;
+        if (valid) {
+            status = HttpStatus.OK;
+        } else{
+            status = HttpStatus.UNAUTHORIZED;
+        }
+
+        return ResponseEntity.status(status).body(valid);
+    }
+
+}
