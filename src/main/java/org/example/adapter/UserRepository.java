d55515b2e52a1bb70b6ed0ff0e7352c094303f8a
eglito <aleeglito@gmail.com>
2025-02-07 21:13:25 -0300

    Criacao da interface UserRepository
    Interface implementa JpaRepository<>. Faz a comunicao com o banco de dados

---
+++ src/main/java/org/example/adapter/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.adapter;
+
+import org.example.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    public Optional<UserEntity> findByLogin(String login);
+}
